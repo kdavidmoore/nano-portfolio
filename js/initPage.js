ba9cd5e06ebcfd01ad3bbe9d15c80313753aab8b
Keith Moore <keith.m4@gmail.com>
2016-09-26 16:23:34 -0400

    added projects from old portfolio

---
+++ js/initPage.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+  // binds localSroll to all local links
+  $.localScroll();
+});
+
+$(window).load(function(){
+  // make all the thumbnail images the same height (my screenshots are not perfect)
+  $('.projects-thumb').matchHeight({
+    byRow: false,
+    property: 'height',
+    target: null,
+    remove: false
+  });
+  // show more info on mouseover
+  $(".thumb-wrapper").mouseenter(function(){
+      $(this).find('.hover-thumb').fadeToggle("fast");
+  });
+  // hide info on mouseleave
+  $(".thumb-wrapper").mouseleave(function(){
+      $(this).find('.hover-thumb').fadeToggle("fast");
+  });
+  // set the thumb-wrapper heights equal to the height of the first image
+  var setHeight = $("#img-1").height();
+  $('.thumb-wrapper').height(setHeight);
+  // when the window is resized, tell matchHeight to update
+  $(window).resize(function() {
+    $.fn.matchHeight._update();
+  });
+});
